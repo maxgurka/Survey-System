5f2a6ecd82b84017c225cc41e06708cf12bc3cc1
20magr06 <max.grindeback@outlook.com>
2023-07-31 19:36:50 +0200

    doughnut charts and chart toggle animation

---
+++ Tengella.Survey.WebApp/wwwroot/js/Visualizations/BarChart.js
@@ -0,0 +1,30 @@
+﻿new Chart("bar_" + passId, {
+    type: "horizontalBar",
+    data: {
+        labels: passLabels,
+        datasets: [{
+            backgroundColor: '#1e87e680',
+            data: passData,
+            barThickness: 50
+        }]
+    },
+    options: {
+        legend: { display: false },
+        scales: {
+            xAxes: [{
+                display: true,
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 1,
+                    precision: 0
+                },
+            }],
+            yAxes: [{
+                // Set label width. Will keep the charts the same size but will truncate long labels
+                afterFit: function (scaleInstance) {
+                    scaleInstance.width = 150;
+                }
+            }]
+        }
+    }
+});
---
+++ Tengella.Survey.WebApp/wwwroot/js/Visualizations/DoughnutChart.js
@@ -0,0 +1,31 @@
+﻿new Chart("doughnut_" + passId, {
+    type: "doughnut",
+    data: {
+        labels: passLabels,
+        datasets: [{
+            backgroundColor: [
+                '#1e87e680'
+                ,'#42f58180'
+                ,'#e042f580'
+                ,'#cbf54280'
+                ,'#f5425180'
+                ,'#4251f580'
+                ,'#4bf54280'
+                ,'#f542b080'
+                ,'#f5964280'
+                ,'#42f5f280'],
+            data: passData,
+            barThickness: 50
+        }]
+    },
+    options: {
+        legend: { display: true },
+        scales: { display: false },
+        yAxes: [{
+            // Set label width. Will keep the charts the same size but will truncate long labels
+            afterFit: function (scaleInstance) {
+                scaleInstance.height = 150;
+            }
+        }]
+    }
+});
