ee046ceec1aa11da5dab9e45ea02e020695d7940
20magr06 <max.grindeback@outlook.com>
2023-08-13 21:42:08 +0200

    survey list is now start page

--- Tengella.Survey.WebApp/Program.cs
+++ Tengella.Survey.WebApp/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Survey}/{action=List}/{id?}");
 
 app.Run();
